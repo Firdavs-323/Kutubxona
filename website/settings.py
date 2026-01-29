d375b5dc38160e2a76148f11e3928da3586e2186
Firdavs <firdavs323@gmail.com>
2026-01-29 18:45:14 +0500

    izoh

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0b50-ic327i5%(52-m#-i%x+zq9^t#f0y894415@vo601zjo#j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['kutubxonafirdavs.pythonanywhere.com']
 
 
 # Application definition
